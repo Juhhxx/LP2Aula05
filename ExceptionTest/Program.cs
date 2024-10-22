3250aa086195a6f085c3e218431319a14d5f5a35
Julia Costa <juliacosta232005@gmail.com>
2024-10-22 16:28:25 +0100

    Add Exception Catches

---
+++ ExceptionTest/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ExceptionTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write a number:");
+            string istr = Console.ReadLine();
+            try
+            {
+                int i = Convert.ToInt32(istr);
+                Console.WriteLine($"Number inserted: {i}");
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (OverflowException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Thank you for using this program");
+            }
+        }
+    }
+}
